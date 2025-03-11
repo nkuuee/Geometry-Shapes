3e0b3113197d9b43da24119ab9eb389ca50c51f1
Nick <nkuuee@bk.ru>
2025-03-11 10:10:12 +0300

    -

--- src/test/java/CircleTest.java
+++ src/test/java/CircleTest.java
@@ -1,3 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CircleTest {
 
+    @Test
+    void testCalculateSquare() {
+        Circle circle = new Circle("Square",2);
+        double expected = 12.56;
+        double actual = circle.calculateSquare();
+        assertEquals(expected,actual,0.01);
+    }
+
+    @Test
+    void testCalculatePerimeter() {
+        Circle circle1 = new Circle("Perimeter",2);
+        double expected = 12.56;
+        double actual = circle1.calculatePerimeter();
+        assertEquals(expected,actual,0.01);
+    }
 }
